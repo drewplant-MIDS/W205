b4dd24d832c38c924e9daf6885e02e0aa72a9c88
Drew Plant <drewplant@ischool.berkeley.edu>
2015-10-16 06:36:57 -0700

    Updated from today's recent edits on AWS. (Oct. 16, 2015)

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -2,4 +2,9 @@
 # mkdir
 #  Only do this if it hasn't already been done...
 # hdfs dfs -mkdir /user/w205/hospital_compare
-hdfs dfs -put /data/w205/hospitalData/strippedFiles/*.csv /user/w205/hospital_compare/
+cd /data/w205/hospitalData/strippedFiles/
+for File in `ls`
+	do
+	echo $File
+	hdfs dfs -put $File /user/w205/hospital_compare/
+	done
